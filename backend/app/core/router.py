c6dd717fd56ce6988934aefb3494474cbfe99d65
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-07 15:01:22 +0300

    feat(purchase-list): register api router

--- backend/app/core/router.py
+++ backend/app/core/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.modules.api.meal_router import router as meal_router
 from app.modules.api.meal_plan_router import router as meal_plan_router
 from app.modules.api.purchase_checklist_router import router as purchase_checklist_router
+from app.modules.api.purchase_list_router import router as purchase_list_router
 
 
 router = APIRouter()
@@ -11,6 +12,7 @@ router = APIRouter()
 router.include_router(meal_router)
 router.include_router(meal_plan_router)
 router.include_router(purchase_checklist_router)
+router.include_router(purchase_list_router)
 
 
 @router.get("/")
