df5918324d3ef792b8672a2074996ee1b07c5358
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-07 17:54:20 +0300

    feat: add frontend providers foundation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+import App from "./app/App";
+
+const queryClient = new QueryClient();
+
+const theme = createTheme();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
