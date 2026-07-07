3cc81a764d5ac113048d139a95e4245347781d54
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-07 17:11:42 +0300

    feat: register purchase dashboard router

--- backend/app/core/router.py
+++ backend/app/core/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.modules.api.meal_router import router as meal_router
 from app.modules.api.meal_plan_router import router as meal_plan_router
 from app.modules.api.purchase_checklist_router import router as purchase_checklist_router
+from app.modules.api.purchase_dashboard_router import router as purchase_dashboard_router
 from app.modules.api.purchase_list_router import router as purchase_list_router
 
 
@@ -13,6 +14,7 @@ router.include_router(meal_router)
 router.include_router(meal_plan_router)
 router.include_router(purchase_checklist_router)
 router.include_router(purchase_list_router)
+router.include_router(purchase_dashboard_router)
 
 
 @router.get("/")
