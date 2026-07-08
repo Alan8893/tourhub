8c9c540474a56850203e6a90d8c1959bd6381471
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-08 16:03:11 +0300

    feat: add project workspace page

---
+++ frontend/src/pages/ProjectWorkspacePage.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+import ProjectWorkspace from "../features/project-workspace/ProjectWorkspace";
+
+export default function ProjectWorkspacePage() {
+  return (
+    <Box sx={{ mt: 4 }}>
+      <ProjectWorkspace />
+    </Box>
+  );
+}
