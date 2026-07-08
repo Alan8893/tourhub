2f3ae60b0e7b4d927b3d6f8aa82e786fe1a58607
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-08 16:11:16 +0300

    feat: register project api router

--- backend/app/core/router.py
+++ backend/app/core/router.py
@@ -6,6 +6,7 @@ from app.modules.api.meta_router import router as meta_router
 from app.modules.api.purchase_checklist_router import router as purchase_checklist_router
 from app.modules.api.purchase_dashboard_router import router as purchase_dashboard_router
 from app.modules.api.purchase_list_router import router as purchase_list_router
+from app.modules.projects.router import router as project_router
 
 
 router = APIRouter(prefix="/api/v1")
@@ -17,6 +18,7 @@ router.include_router(meta_router)
 router.include_router(purchase_checklist_router)
 router.include_router(purchase_list_router)
 router.include_router(purchase_dashboard_router)
+router.include_router(project_router)
 
 
 @router.get("/")
