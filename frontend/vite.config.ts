bb5cb68f42d52ee636c113873693d0106f526eeb
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-08 21:48:40 +0300

    fix(frontend): avoid node path dependency in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,3 @@
-import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': '/src',
     },
   },
   server: {
