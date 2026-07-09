df55f188da6a94c595f7233323e430485a4c0dc6
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-09 22:37:26 +0300

    chore: merge alembic heads

---
+++ backend/alembic/versions/d10001_merge_heads.py
@@ -0,0 +1,20 @@
+"""merge alembic heads
+
+Revision ID: d10001
+Revises: a40001, c10001
+Create Date: 2026-07-09
+"""
+
+
+revision = "d10001"
+down_revision = ("a40001", "c10001")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
