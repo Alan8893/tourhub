dcd18a5cfff5a88c71ff681c45b0957415b38f7b
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-13 18:25:30 +0300

    feat: improve dish selector

--- frontend/src/features/meal-slot/components/DishSelector.tsx
+++ frontend/src/features/meal-slot/components/DishSelector.tsx
@@ -3,24 +3,24 @@ import { MenuItem, Select } from "@mui/material";
 import { useDishes } from "../hooks/useDishes";
 
 interface DishSelectorProps {
-  value?: string;
+  value: string;
   onChange: (dishId: string) => void;
 }
 
-export default function DishSelector({
-  value,
-  onChange,
-}: DishSelectorProps) {
+export default function DishSelector({ value, onChange }: DishSelectorProps) {
   const { data: dishes = [] } = useDishes();
 
   return (
     <Select
-      value={value ?? ""}
+      size="small"
+      value={value}
       displayEmpty
-      onChange={(event) => onChange(event.target.value)}
+      onChange={(event) => onChange(String(event.target.value))}
+      sx={{ minWidth: 220 }}
     >
-      <MenuItem value="">Select dish</MenuItem>
-
+      <MenuItem value="">
+        <em>Select dish</em>
+      </MenuItem>
       {dishes.map((dish) => (
         <MenuItem key={dish.id} value={dish.id}>
           {dish.name}
