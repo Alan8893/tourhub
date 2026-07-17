61b9d2693082d1850c205e8df2569687d0ba4ced
Alan8893 <117682360+Alan8893@users.noreply.github.com>
2026-07-17 08:31:04 +0300

    register club settings model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,29 +1,31 @@
 from app.models.base import Base
 
-from app.models.product import ProductORM
-from app.models.ingredient import IngredientORM
-from app.models.recipe import RecipeORM
-from app.models.recipe_component import RecipeComponentORM
-from app.models.recipe_component_type import RecipeComponentType
-from app.models.recipe_equipment_requirement import RecipeEquipmentRequirementORM
+from app.models.club_settings import ClubSettingsORM
 from app.models.dish import DishORM
 from app.models.dish_meal_role import DishMealRoleMealTypeORM, DishMealRoleORM
+from app.models.equipment_list import EquipmentListORM
+from app.models.equipment_list_item import EquipmentListItemORM
+from app.models.ingredient import IngredientORM
 from app.models.meal_plan import MealPlanORM
 from app.models.meal_plan_day import MealPlanDayORM
 from app.models.meal_plan_item import MealPlanItemORM
 from app.models.meal_slot import MealSlotORM
 from app.models.meal_slot_dish import MealSlotDishORM
+from app.models.product import ProductORM
 from app.models.purchase_checklist import PurchaseChecklistORM
 from app.models.purchase_checklist_item import PurchaseChecklistItemORM
 from app.models.purchase_list import PurchaseListORM
 from app.models.purchase_list_item import PurchaseListItemORM
-from app.models.equipment_list import EquipmentListORM
-from app.models.equipment_list_item import EquipmentListItemORM
+from app.models.recipe import RecipeORM
+from app.models.recipe_component import RecipeComponentORM
+from app.models.recipe_component_type import RecipeComponentType
+from app.models.recipe_equipment_requirement import RecipeEquipmentRequirementORM
 from app.modules.projects.models.project import ProjectORM
 
 
 __all__ = [
     "Base",
+    "ClubSettingsORM",
     "ProductORM",
     "IngredientORM",
     "RecipeORM",
